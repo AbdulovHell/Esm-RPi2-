b7b33de4f5bff3005496ce2dda6a083fb814f503
AbdulovHell <dronovova@gmail.com>
2017-07-19 21:09:24 +0300

    lcd test

--- Blink1/main.cpp
+++ Blink1/main.cpp
@@ -1,27 +1,30 @@
 #include <wiringPi.h>
-#include "lcd.h"
+#include <lcd.h>
 
 // Контакт LED — контакт 0 wiringPi равен BCM_GPIO 17.
 // При инициализации с использованием wiringPiSetupSys нужно применять нумерацию BCM
 // При выборе другого ПИН-кода используйте нумерацию BCM, также
 // обновите команду "Страницы свойств" — "События сборки" — "Удаленное событие после сборки". 
 // , которая использует gpio export для настройки значений для wiringPiSetupSys
-#define	LED	17
+#define RW 27
+#define RS 17
+#define E 22
+
+#define DB7 26
+#define DB6 19
+#define DB5 13
+#define DB4 6
 
 int main(void)
 {
 	wiringPiSetupSys();
-
-	//pinMode(LED, OUTPUT);
-	InitLCD();
-
+	pinMode(RW, OUTPUT);
+	digitalWrite(RW, LOW);
+	int fd=lcdInit(16, 2, 4, RS, E, 0, 0, 0, 0, DB4, DB5, DB6, DB7);
+	lcdPrintf(fd, "message");
 	while (true)
 	{
-		ClearDisplay();
-		//digitalWrite(LED, HIGH);  // Вкл.
-		//delay(100); // мс
-		//digitalWrite(LED, LOW);	  // Откл.
-		//delay(100);
+		;
 	}
 	return 0;
 }
