c9f88f384fbba2147939a01544d441ca4eb05178
AbdulovHell <dronovova@gmail.com>
2017-07-28 15:04:39 +0300

    uart

--- Blink1/main.cpp
+++ Blink1/main.cpp
@@ -11,6 +11,8 @@
 
 #include "SPI_DRV.h"
 
+#include <wiringSerial.h>
+
 // Контакт LED — контакт 0 wiringPi равен BCM_GPIO 17.
 // При инициализации с использованием wiringPiSetupSys нужно применять нумерацию BCM
 // При выборе другого ПИН-кода используйте нумерацию BCM, также
@@ -34,20 +36,32 @@ int main()
 	//system("gpio load spi");
 	int ec = wiringPiSetupSys();
 	digitalWrite(RW, LOW);
-
-	if (SPISetup(0, 625000, 0) == -1) {
+	
+	int fd = serialOpen("/dev/ttyAMA0", 9600);
+	if (fd == -1)
+		return 0;
+	/*if (SPISetup(0, 625000, 0) == -1) {
 		printf("error init spi");
 		return 1;
-	}
-	char* rBuf=new char[100];
-	char* wBuf=new char[100];
-	memset(wBuf,'0',100);
-	memset(rBuf, '0', 100);
-	wBuf[0] = '1';
+	}*/
+	/*char* rBuf=new char[25];
+	char* wBuf=new char[25];
+	memset(wBuf,0xf5,25);
+	memset(rBuf, '0', 25);
+	wBuf[0] = 0xF5;*/
 	//int fd = lcdInit(16, 2, 4, RS, E, 0, 0, 0, 0, DB4, DB5, DB6, DB7);
 	//lcdPrintf(fd, "message");
+	int read_cnt=0;
 	while (true)
 	{
+		read_cnt=serialDataAvail(fd);
+		if (read_cnt > 0) {
+			char* buf = new char[read_cnt+1];
+			for (int i = 0; i < read_cnt; i++)
+				buf[i]=serialGetchar(fd);
+			buf[read_cnt] = 0;
+			serialPuts(fd, buf);
+		}
 		//printf("input ");
 		//scanf("%s",buf);
 		//sprintf(wBuf, "asdasdjhakhw");
@@ -55,13 +69,13 @@ int main()
 		//	break;
 		//}
 		//else {
-		int ret = SPIDataRW(0, (unsigned char*)rBuf, (unsigned char*)wBuf, 100);
+		/*int ret = SPIDataRW(0, (unsigned char*)rBuf, (unsigned char*)wBuf, 25);
 		uint32_t sym = 0;
-		for (int i = 0; i < 100; i++)
+		for (int i = 0; i < 25; i++)
 			sym += rBuf[i];
 		if (sym > 0)
 			printf("%s\n", rBuf);
-		//}
+		*///}
 	}
 	return 0;
 }
